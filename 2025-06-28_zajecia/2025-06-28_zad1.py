c246ef8daf4846e067a8c931855e28fe5e903b82
Przemysław Górski <przemyslaw.gorski@gmail.com>
2025-06-28 09:52:59 +0200

    Api quiz historyczny

--- 2025-06-28_zajecia/2025-06-28_zad1.py
+++ 2025-06-28_zajecia/2025-06-28_zad1.py
@@ -1,1 +1,31 @@
 #API
+
+import requests
+
+def pytanie():
+    url = "http://numbersapi.com/random/year?json"
+    response = requests.get(url)
+
+    data = response.json()
+    tekst = data["text"]
+    rok = data["number"]
+    print(rok)
+    return tekst, rok
+
+def quiz():
+    print("🧩 Witaj w quizie .")
+    tekst, poprawna_odpowiedz =pytanie()
+
+    tekst_z_luka = tekst.replace(str(poprawna_odpowiedz), "___")
+    print("\n📜 Ciekawostka:")
+    print(tekst_z_luka)
+
+    try:
+        odpowiedz = int(input("\n🔢 Twoja odpowiedź: "))
+        if odpowiedz == poprawna_odpowiedz:
+            print("✅ Brawo! To poprawna odpowiedź.")
+        else:
+            print(f"❌ Pudło! Chodziło o rok {poprawna_odpowiedz}.")
+    except ValueError:
+        print("⚠️ Wprowadź poprawną liczbę.")
+quiz()
